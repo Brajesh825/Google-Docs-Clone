8c9b252e49d02258eb7c05c9a46d6a3f7f0f55f1
brajeshmishra825 <brajeshmishra825@gmail.com>
2022-06-01 16:39:08 +0530

    Completed the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Editor from './component/Editor'
+import "./App.css"
 
 function App ()  {
   return (
--- client/src/component/Editor.jsx
+++ client/src/component/Editor.jsx
@@ -1,45 +1,49 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-import Quill from 'quill'
-import 'quill/dist/quill.snow.css'
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
 
-import {Box} from '@mui/material'
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+const Component = styled.div`
+  background : #f6f6f6 ;
+`;
+
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
 
 const Editor = () => {
-    useEffect(()=>{
-
-        const toolbarOptions = [
-            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-            ['blockquote', 'code-block'],
-          
-            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-            [{ 'direction': 'rtl' }],                         // text direction
-          
-            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-          
-            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-            [{ 'font': [] }],
-            [{ 'align': [] }],
-          
-            ['clean']                                         // remove formatting button
-          ];
-
-        const quill = new Quill('#container', {
-            theme: 'snow',
-            modules:{ 
-                toolbar : toolbarOptions
-            }
-        });
-
-    },[]) // Component did mount
+  useEffect(() => {
+    const quill = new Quill("#container", {
+      theme: "snow",
+      modules: {
+        toolbar: toolbarOptions,
+      },
+    });
+  }, []); // Component did mount
 
   return (
-    <Box id="container"></Box>
-  )
-}
+    <Component>
+      <Box className="container" id="container"></Box>
+    </Component>
+  );
+};
 
-export default  Editor
+export default Editor;
